1953adfd7c8b07e9e4e85883856dccddf0e4fb14
Marc23444 <36685499+Marc23444@users.noreply.github.com>
2021-09-14 22:12:04 -0400

    Initial Commit

---
+++ Exercise06/src/main/java/MainSolution/Solution06.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+package MainSolution;
+
+public class Solution06 {
+    public static void main(String[] args) {
+
+    }
+}
